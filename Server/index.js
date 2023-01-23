dd26f4ff02a40d6d6e2432bd8e1d7f3bea74f63b
gergof2 <heroghost20001024@gmail.com>
2023-01-23 16:17:34 +0100

    Backend : Create : Express Server (index.js)

---
+++ Server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const {Server} = require("socket.io");
+const helmet = require("helmet");
+
+const app = express();
+const server = require("http").createServer(app);
+const io = new Server();
+
+app.use(helmet());
+app.use(express.json());
+
+server.listen(4000, () => {
+    console.log("Server is listening on port 4000!")
+});
