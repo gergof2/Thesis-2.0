911c8d2ec0f0e1a23b9414c6082b05f7f51bfa80
gergof2 <heroghost20001024@gmail.com>
2023-01-24 20:14:06 +0100

    Backend : Update : Add userRouter to the server

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const {Server} = require("socket.io");
 const helmet = require("helmet");
+const userRouter = require("./api/User");
+
 
 const app = express();
 const server = require("http").createServer(app);
@@ -8,6 +10,8 @@ const io = new Server();
 
 app.use(helmet());
 app.use(express.json());
+
+app.use("/user", userRouter);
 
 server.listen(4000, () => {
     console.log("Server is listening on port 4000!")
