62d2f0e16b1f6efbf8e0e2c92c06d5d98e857a3c
gergof2 <heroghost20001024@gmail.com>
2023-01-24 20:10:30 +0100

    Backend : Update : Add e-mail checker to userModel

--- Server/model/userModel.js
+++ Server/model/userModel.js
@@ -2,6 +2,11 @@ const express = require('express');
 const pool = require('../config/db.js');
 
 class user_model{
+
+    static async checkEmail(email){
+        const response = await pool.query("SELECT username FROM users WHERE email = $1", [email]);
+        return response;
+    }
 }
 
 module.exports = user_model;
