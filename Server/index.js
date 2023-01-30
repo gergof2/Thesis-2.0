fb12719bf9ff2bc90f2aa533dff7bb8579417cf2
gergof2 <heroghost20001024@gmail.com>
2023-01-30 13:14:45 +0100

    Backend : Update : Create session

--- Server/index.js
+++ Server/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const {Server} = require("socket.io");
 const helmet = require("helmet");
 const userRouter = require("./api/User");
-
+const session = require('express-session');
+require("dotenv").config();
 
 const app = express();
 const server = require("http").createServer(app);
@@ -10,6 +11,21 @@ const io = new Server();
 
 app.use(helmet());
 app.use(express.json());
+
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    credentials: true,
+    name: "session_id",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: "auto",
+        httpOnly: true,
+        expires: 1000 * 60 * 60 * 54 * 7,
+        sameSite: "lax",        
+    },   
+})
+);
 
 app.use("/user", userRouter);
 
