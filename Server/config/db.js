b390c25adcf70660f8bd02ffbd49d90d1d3bc2f9
gergof2 <heroghost20001024@gmail.com>
2023-01-30 13:14:24 +0100

    Backend : Update : Add connection information to server

--- Server/config/db.js
+++ Server/config/db.js
@@ -9,4 +9,12 @@ const pool = new Pool({
     port: 5432,
 });
 
+pool.connect((err) => {
+    if (err) {
+      console.log("Connect to database... error: ", err);
+    } else {
+      console.log("Connected to database!");
+    }
+});
+
 module.exports = pool;
